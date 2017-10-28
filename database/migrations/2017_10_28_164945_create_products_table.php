a7c1ed46d695e5148c809249257adfffa936df7d
unknown <cicero_roniel2hotmail.com>
2017-10-28 19:17:37 -0300

    VP2

---
+++ database/migrations/2017_10_28_164945_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome', 40);
+            $table->string('descricao')->nullable();
+            $table->float('quantidade')->nullable();
+            $table->float('preco', 8,2)->default(0);
+            $table->string('fornecedor', 40)->nullable();
+            $table->string('saida');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
